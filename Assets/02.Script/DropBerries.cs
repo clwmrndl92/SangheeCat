016a5275ccb6e09b7079c935be3a0aed8d531810
clwmrndl92 <clwmrndl92@gmail.com>
2024-08-24 03:41:09 +0900

    엔딩씬

--- Assets/02.Script/DropBerries.cs
+++ Assets/02.Script/DropBerries.cs
@@ -8,15 +8,34 @@ public class DropBerries : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var berry in berries)
+        {
+            berry.transform.localScale = Random.Range(0.7f, 1) * Vector3.one;
+        }
+        StartCoroutine("DropBerryRoutine");
     }
 
-    public void Force()
+    public void Drop()
     {
         foreach (var berry in berries)
         {
             berry.Force();
         }
         
+    }
+    public void DropCarrot()
+    {
+        foreach (var berry in berries)
+        {
+            berry.Force(-20, 20, 0, 20);
+        }
+        
+    }
+
+    IEnumerator DropBerryRoutine() {
+        while(true) {
+            DropCarrot();
+            yield return new WaitForSeconds(2);
+        }
     }
 }
