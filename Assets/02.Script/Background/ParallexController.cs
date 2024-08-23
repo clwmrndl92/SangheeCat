9182b25e19472501df32682a14fec7f46adbb1fa
clwmrndl92 <clwmrndl92@gmail.com>
2024-08-24 05:16:06 +0900

    패럴렉스

--- Assets/02.Script/Background/ParallexController.cs
+++ Assets/02.Script/Background/ParallexController.cs
@@ -32,8 +32,8 @@ namespace SangheeCat{
             
             foreach (var layer in layers)
             {
-                layer.startPosX = layer.layerObject.transform.position.x - (_cam.transform.position.x * layer.parallaxFactor);
-                // layer.length = layer.layerObject.GetComponent<SpriteRenderer>().bounds.size.x;
+                layer.startPosX = layer.layerObject.transform.position.x;
+                layer.length = layer.layerObject.GetComponent<SpriteRenderer>().bounds.size.x;
 
                 layer.startSize = layer.layerObject.transform.localScale;
                 
@@ -44,13 +44,12 @@ namespace SangheeCat{
             float zoom = _cam.orthographicSize / startZoomSize;
             foreach (var layer in layers)
             {
-                float temp = (_cam.transform.position.x * (1-layer.parallaxFactor));
-                float dist = (_cam.transform.position.x * layer.parallaxFactor);
+                float speed = layer.parallaxFactor * Time.deltaTime;
                 
                 // Disable movement
-                //layer.layerObject.transform.position = new Vector3(layer.startPosX + dist, layer.layerObject.transform.position.y, layer.layerObject.transform.position.z);
-                // if (temp > layer.startPosX + layer.length) layer.startPosX += 2*layer.length;
-                // else if (temp < layer.startPosX - layer.length) layer.startPosX -= 2*layer.length;
+                layer.layerObject.transform.position = new Vector3(layer.layerObject.transform.position.x - speed, layer.layerObject.transform.position.y, layer.layerObject.transform.position.z);
+                if (layer.layerObject.transform.position.x > layer.startPosX + layer.length || layer.layerObject.transform.position.x < layer.startPosX - layer.length) 
+                    layer.layerObject.transform.position = new Vector3(layer.startPosX, layer.layerObject.transform.position.y, layer.layerObject.transform.position.z);
 
                 
                 layer.layerObject.transform.localScale =  layer.startSize + layer.startSize * (zoom-1) * layer.zoomFactor;
